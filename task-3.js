c8f825b682657d0262b0dc19dee8a9f8ed9a6e48
Alexandra100layrenko <66309809+Alexandra100layrenko@users.noreply.github.com>
2023-10-17 11:24:40 +0100

    update all

--- task-3.js
+++ task-3.js
@@ -1,1 +1,13 @@
+function getElementWidth(content, padding, border){
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding);
+    const borderWidth = parseFloat(border);
+   
+    const totalWidth = content + 2 * padding + 2 * border;
 
+    return totalWidth;
+}
+
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
