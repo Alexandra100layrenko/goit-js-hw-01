f32fef092b8245b92a6926ea74c851d1f1f81cf9
Alexandra100layrenko <66309809+Alexandra100layrenko@users.noreply.github.com>
2023-10-17 10:19:07 +0100

    update all

--- task-1.js
+++ task-1.js
@@ -1,1 +1,20 @@
+function makeTransaction (quantity, pricePerDroid){
+    const message;
+    const totalPrice = "quantity * pricePerDroid";
+  
+    switch (makeTransaction) {
+        case ("5") :
+            message = `You odered ${quantity} droids worth ${totalPrice} credits!`
+            break;
+        case ("3") :
+            message = `You odered ${quantity} droids worth ${totalPrice} credits!`
+            break;
+        case ("10") :
+            message = `You odered ${quantity} droids worth ${totalPrice} credits!`
+            break;
+    }
+}
 
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
